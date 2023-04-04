f69e068c66ddaf39f2c4164f44fbd316cb52584d
JunKyu Choi <jkchoi.dev@gmail.com>
2023-04-05 01:42:09 +0900

    update playjs

---
+++ Lab Assignment/#6/playjs.js
@@ -0,0 +1,6 @@
+function calc(){
+	var x = document.getElementById("x");
+	var y = document.getElementById("y");
+	var sum = document.getElementById("sum");
+	sum.value = Number(x.value)+Number(y.value);
+}
