2d3355b36f0cc0779924366bc8e16a086cc7ca1c
chemdise <122720798+junkyn@users.noreply.github.com>
2023-04-05 16:52:49 +0900

    assignment #6
    update number quiz

--- Lab Assignment/#6/playjs.js
+++ Lab Assignment/#6/playjs.js
@@ -4,3 +4,21 @@ function calc(){
 	var sum = document.getElementById("sum");
 	sum.value = Number(x.value)+Number(y.value);
 }
+var computerNumber = 53;
+var nGuesses=0;
+function guess(){
+	var ipNum = document.getElementById("user");
+	var count = document.getElementById("guesses");
+	var hint = document.getElementById("result");
+	nGuesses++;
+	count.value = nGuesses;
+	if(ipNum.value>computerNumber){
+		hint.value="낮습니다";
+	}else if(ipNum.value<computerNumber){
+		hint.value="높습니다.";
+	}else{
+		hint.value="정답!"
+		nGuesses=0;
+	}
+
+}
