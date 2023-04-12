7fb29620c7d124717c5ee2e1ccf985bded3cb7dd
sterdsterd <sterdsterd@icloud.com>
2023-04-12 23:45:23 +0900

    Feat: UserInfo Class 추가

---
+++ src/kucse/introductoryproject/b01/UserInfo.java
@@ -0,0 +1,19 @@
+package kucse.introductoryproject.b01;
+
+public class UserInfo {
+    private String id;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+    private String birthday;
+
+    public UserInfo(String id, String password, String name, String phone, String address, String birthday) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.birthday = birthday;
+    }
+}
