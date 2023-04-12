39e064738b0f89b4060f4f28d993299746dcc094
sterdsterd <sterdsterd@icloud.com>
2023-04-13 00:13:38 +0900

    Feat: parseUserDataFromCSV 메서드 구현

--- src/kucse/introductoryproject/b01/Main.java
+++ src/kucse/introductoryproject/b01/Main.java
@@ -1,10 +1,17 @@
 package kucse.introductoryproject.b01;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, UserInfo> userInfoHashMap = UserInfo.parseUserDataFromCSV(new File("dummyUserData.csv"));
+
+        for (String key : userInfoHashMap.keySet()) {
+            System.out.println(key + " : " + userInfoHashMap.get(key));
+        }
+
         HashSet<Contact> contactArrayList = Contact.parseContactsFromCSV(new File("dummyContacts.csv"));
 
         for (Contact it : contactArrayList) {
--- src/kucse/introductoryproject/b01/UserInfo.java
+++ src/kucse/introductoryproject/b01/UserInfo.java
@@ -1,5 +1,11 @@
 package kucse.introductoryproject.b01;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class UserInfo {
     private String id;
     private String password;
@@ -15,5 +21,35 @@ public class UserInfo {
         this.phone = phone;
         this.address = address;
         this.birthday = birthday;
+    }
+
+    public static HashMap<String, UserInfo> parseUserDataFromCSV(File file) {
+        HashMap<String, UserInfo> userInfoHashMap = new HashMap<>();
+
+        try (Scanner fileScanner = new Scanner(file)){
+            while(fileScanner.hasNextLine()) {
+                String str = fileScanner.nextLine();
+                String[] userInfo = str.split("\t");
+
+                userInfoHashMap.put(userInfo[0].trim(), new UserInfo(userInfo[0].trim(), userInfo[1].trim(), userInfo[2].trim(), userInfo[3].trim(), userInfo[4].trim(), userInfo[5].trim()));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return userInfoHashMap;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday='" + birthday + '\'' +
+                '}';
     }
 }
