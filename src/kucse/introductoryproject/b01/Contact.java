7f9c9caf9154ba1ff59f170035503aa742b43e9b
sterdsterd <sterdsterd@icloud.com>
2023-04-12 23:43:43 +0900

    Feat: Contact Class 추가

---
+++ src/kucse/introductoryproject/b01/Contact.java
@@ -0,0 +1,18 @@
+package kucse.introductoryproject.b01;
+
+public class Contact {
+    private String name;
+    private String phone;
+    private String address;
+    private String birthday;
+    private String memo;
+
+    public Contact(String name, String phone, String address, String birthday, String memo) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.birthday = birthday;
+        this.memo = memo;
+    }
+
+}
