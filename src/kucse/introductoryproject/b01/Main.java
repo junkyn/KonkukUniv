d137ff11ddc78adc795c52f11e36ec97eb842045
Hwan616 <hoan0616@naver.com>
2023-04-13 13:40:26 +0900

    Feat: login 메서드 구현

--- src/kucse/introductoryproject/b01/Main.java
+++ src/kucse/introductoryproject/b01/Main.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
+    static HashMap<String, UserInfo> userInfoHashMap = UserInfo.parseUserDataFromCSV(new File("dummyUserData.csv"));
     public static void main(String[] args) {
-        HashMap<String, UserInfo> userInfoHashMap = UserInfo.parseUserDataFromCSV(new File("dummyUserData.csv"));
-
         for (String key : userInfoHashMap.keySet()) {
             System.out.println(key + " : " + userInfoHashMap.get(key));
         }
@@ -17,5 +16,22 @@ public class Main {
         for (Contact it : contactArrayList) {
             System.out.println(it);
         }
+    }
+
+    public static UserInfo login(String id, String pw) {
+        for (String key : userInfoHashMap.keySet()) {
+            if (key.equals(id)) {
+                if (userInfoHashMap.get(key).getPassword().equals(pw)) {
+                    String name = userInfoHashMap.get(key).getName();
+                    System.out.println(name + "님 환영합니다\n");
+                    return userInfoHashMap.get(key);
+                } else {
+                    System.out.println("비밀번호가 틀립니다\n");
+                    return null;
+                }
+            }
+        }
+        System.out.println("존재하지 않는 아이디입니다\n");
+        return null;
     }
 }
