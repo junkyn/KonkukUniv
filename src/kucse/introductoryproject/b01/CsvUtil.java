8a3fc1c7b8a0d3345352bb6eebe4f5075ef2e3d3
sterdsterd <sterdsterd@icloud.com>
2023-04-13 13:38:56 +0900

    Feat: CsvUtil (CSV로 저장하는 Util) 구현

---
+++ src/kucse/introductoryproject/b01/CsvUtil.java
@@ -0,0 +1,40 @@
+package kucse.introductoryproject.b01;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CsvUtil {
+    private static FileWriter fileWriter;
+
+    public static void init(String fileName) {
+        File contactFile;
+        try {
+            contactFile = new File(fileName + ".csv");
+
+            if (!contactFile.exists()) contactFile.createNewFile();
+
+            fileWriter = new FileWriter(contactFile, true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeContact(Contact contact) {
+        try {
+            fileWriter.write(contact.toCsv());
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeStatisticWriterStream() {
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
