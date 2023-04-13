12254c1a5f9d8b371f6f961df7e35b2cdf4c6dec
JunKyu Choi <jkchoi.dev@gmail.com>
2023-04-13 13:08:36 +0900

    이름 입력 기능

--- src/kucse/introductoryproject/b01/addressBook.java
+++ src/kucse/introductoryproject/b01/addressBook.java
@@ -16,15 +16,20 @@ public class addressBook {
     public void addContact(){
         String name,phone,address,birthday,memo;
         System.out.println("(Skip을 원하시면 Enter을 눌러주세요. 단, 이름과 전화번호는 필수");
-        System.out.println("이름을 입력하세요");
-        System.out.print(">");
         while(true){
+            System.out.println("이름을 입력하세요");
+            System.out.print(">");
             name=s.nextLine();
             if(name.equals("\n"))
                 System.out.println("이름은 필수 입력입니다");
             else if(isInteger(name))
-
+                System.out.println("정수로 이뤄진 이름은 불가능합니다");
+            else if(name.contains("\t"))
+                System.out.println("탭(tab)은 사용하실 수 없습니다");
+            else
+                break;
         }
+
     }
     private boolean isInteger(String s){
         try{
