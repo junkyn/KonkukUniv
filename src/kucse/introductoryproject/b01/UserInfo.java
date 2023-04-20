6c52e8e35b3de8aadbde760d3e7e570e55b59bc8
sterdsterd <sterdsterd@icloud.com>
2023-04-21 04:09:42 +0900

    Feat: Update UserInfo Setter Methods

--- src/kucse/introductoryproject/b01/UserInfo.java
+++ src/kucse/introductoryproject/b01/UserInfo.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class UserInfo {
     private String id;
@@ -14,52 +15,8 @@ public class UserInfo {
     private String address;
     private String birthday;
 
-    public String getId() {
-        return id;
-    }
+    public UserInfo() {
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
     }
 
     public UserInfo(String id, String password, String name, String phone, String address, String birthday) {
@@ -69,6 +26,106 @@ public class UserInfo {
         this.phone = phone;
         this.address = address;
         this.birthday = birthday;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public boolean setId(String id) {
+        if (id.length() < 5)
+            System.out.println("ID의 길이는 5 이상이어야 합니다.");
+        else if (id.matches("^[a-zA-Z0-9]*$"))
+            System.out.println("ID는 알파벳 또는 숫자로만 이루어져야 합니다.");
+        else {
+            this.id = id;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean setPassword(String password) {
+        if (password.length() < 8)
+            System.out.println("암호의 길이는 8 이상이어야 합니다.");
+        else if (password.matches(".*\\s.*"))
+            System.out.println("암호는 공백 또는 탭을 포함할 수 없습니다.");
+        else {
+            this.password = password;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean setName(String name) {
+        if (name.isBlank())
+            System.out.println("이름은 필수 입력입니다");
+        else if (name.chars().allMatch(Character::isDigit))
+            System.out.println("정수로만 이루어진 이름은 사용할 수 없습니다");
+        else if (name.contains("\t"))
+            System.out.println("탭(tab)은 사용하실 수 없습니다");
+        else {
+            this.name = name;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean setPhone(String phone) {
+        if (phone.isBlank())
+            System.out.println("전화번호는 필수 입력입니다");
+        else if (phone.contains("\t"))
+            System.out.println("탭(tab)은 사용하실 수 없습니다");
+        else {
+            this.phone = phone;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean setAddress(String address) {
+        if (address.contains("\t"))
+            System.out.println("탭(tab)은 사용하실 수 없습니다");
+        else {
+            this.address = address;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean setBirthday(String birthday) {
+        if (address.contains("\t"))
+            System.out.println("탭(tab)은 사용하실 수 없습니다");
+        else {
+            this.birthday = birthday;
+            return true;
+        }
+
+        return false;
     }
 
     public static HashMap<String, UserInfo> parseUserDataFromCSV(File file) {
