3e59a6a2c72823def563ecd0d44f94f3ce312e93
JunKyu Choi <jkchoi.dev@gmail.com>
2023-04-21 05:10:57 +0900

    feat : view(string), edit, delete

--- src/kucse/introductoryproject/b01/AddressBook.java
+++ src/kucse/introductoryproject/b01/AddressBook.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-    private Contact OnContact;
+    private Contact onContact;
     private static Scanner scanner = new Scanner(System.in);
 
     private HashSet<Contact> contactSet;
@@ -48,6 +48,7 @@ public class AddressBook {
         viewAddressBook(1);
     }
     public void viewAddressBook(int page) { // view (page)
+        resetOnContact();
         List<Contact> list = new ArrayList<>(contactSet);
         int maxPage = (list.size() - 1) / 10 + 1;
         if (list.isEmpty()) {
@@ -65,7 +66,76 @@ public class AddressBook {
             System.out.println("--------(" + page + "/" + maxPage + ")--------");
         }
     }
+    public  void viewAddressBook(String name){
+        onContact = contactSet.stream().filter(it -> it.getName().equals(name)).findFirst().orElseGet(null);
+        if(onContact==null){
+            System.out.println("존재하지 않는 연락처입니다");
+            return;
+        }
+        else{
+            System.out.println(onContact);
+            System.out.println();
+            System.out.println(onContact.getName()+">");
+        }
 
+    }
+    public void editContact(){
+        if(onContact==null){
+            System.out.println("수정할 연락처를 열람해주세요");
+            return;
+        }else{
+            String order;
+            String value;
+            do{
+                System.out.println("수정할 부분을 입력해주세요");
+                System.out.println("(name : 이름, num : 전화번호, address : 주소, birth : 생년월일, memo : 메모, cancel : 취소");
+                System.out.println(onContact.getName()+">edit>");
+                order = scanner.nextLine();
+                if(order.equals("name")){
+                    do System.out.println("수정할 내용을 입력해주세요\n"+onContact.getName()+"edit>name>");
+                    while (!onContact.setName(renameDuplicatedName(scanner.nextLine().trim())));
+                }
+                else if(order.equals("num")){
+                    do System.out.println("수정할 내용을 입력해주세요\n"+onContact.getName()+"edit>num>");
+                    while (!onContact.setPhone(scanner.nextLine().trim(), this));
+                }
+                else if(order.equals("address")){
+                    do System.out.println("수정할 내용을 입력해주세요\n"+onContact.getName()+"edit>address>");
+                    while (!onContact.setAddress(scanner.nextLine().trim()));
+                }
+                else if(order.equals("birth")){
+                    do System.out.println("수정할 내용을 입력해주세요\n"+onContact.getName()+"edit>birth>");
+                    while (!onContact.setBirthday(scanner.nextLine().trim()));
+                }
+                else if(order.equals("memo")){
+                    do System.out.println("수정할 내용을 입력해주세요\n"+onContact.getName()+"edit>memo>");
+                    while (!onContact.setMemo(scanner.nextLine().trim()));
+                }
+            }while(order.equals("cancel"));
+        }
+    }
+    public void deleteContact(){
+        if(onContact==null){
+            System.out.println("삭제할 연락처를 열람해주세요");
+            return;
+        }
+        else{
+            String answer;
+            System.out.println("정말 삭제하시겠습니까? 삭제를 원하시면 '삭제'를 입력해주세요\n"+onContact.getName()+">delete>");
+            answer = scanner.nextLine();
+            if(answer.equals("삭제")){
+                contactSet.remove(onContact);
+                onContact = null;
+                System.out.println("삭제되었습니다.");
+            }else{
+                System.out.println("삭제되지 않았습니다");
+            }
+        }
+
+    }
+    public void resetOnContact(){
+        onContact = null;
+    }
     protected boolean isPhoneDuplicated(String n) { // 중복 전화번호
         return contactSet.stream().anyMatch(it -> getNumbersOnly(it.getPhone()).equals(getNumbersOnly(n)));
     }
