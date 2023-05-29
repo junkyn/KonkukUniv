7aca5d1647d7f83021456c2389e1dde493eb05c4
JunKyu Choi <jkchoi.dev@gmail.com>
2023-05-29 16:59:54 +0900

    Update equivalence.cpp

--- Practice/equivalence.cpp
+++ Practice/equivalence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <time.h>
 
 //O(n^2) , Use Stack, DFS
 using namespace std;
@@ -20,16 +21,24 @@ int Pop() {
 int isEmpty() {
 	return (SP == 0);
 }
-void SetLink(int x, int y) { // ¿¬°á~
+void SetLink(int x, int y) { // ì—°ê²°~
 	MAP[x][y] = 1;
 	return;
 }
 int LastForward[1000];
+<<<<<<< Updated upstream
 int NextForward(int x) { // x³ëµå¿Í ¿¬°áµÈ ³ëµå Ã£´Âµ¥ 1ºÎÅÍ ÂùÂùÈ÷ Ã£À½
 	LastForward[x]++; // ÃÊ±â°ª 0ÀÎµ¥ 1ºÎÅÍ´Ï±î ÇÏ³ª Áõ°¡ÇÏ¸é¼­ ½ÃÀÛ
 	while (LastForward[x] <= n) { // ÃÖ´ñ°ªº¸´Ù ÀÛÀ¸¸é~
 		if (MAP[x][LastForward[x]] == 1) // x°¡ ÀÌ°É °¥¼öÀÖ³ª¿ä?
 			return LastForward[x]; //°¥ ¼ö ÀÖ³×
+=======
+int NextForward(int x) { // xë…¸ë“œì™€ ì—°ê²°ëœ ë…¸ë“œ ì°¾ëŠ”ë° 1ë¶€í„° ì°¬ì°¬íˆ ì°¾ìŒ
+	LastForward[x]++;
+	while (LastForward[x] <= n) {
+		if (MAP[x][LastForward[x]] == 1) 
+			return LastForward[x];
+>>>>>>> Stashed changes
 		else
 			LastForward[x]++; // ¸ø°¡³× ´ÙÀ½°Å °Ë»ö -> °á±¹ 1ºÎÅÍ n±îÁö º»´Ù´Â°Å
 		
@@ -46,8 +55,13 @@ void Mark(int x) {
 }
 int NextStart() {
 	LastStart++;
+<<<<<<< Updated upstream
 	while (LastStart <= n) { 
 		if (Visited[LastStart] == 0) // 1ºÎÅÍ ¿Ã¶ó°¡¼­ ¹æ¹®¾ÈÇÑ ³ëµå Å½»ö
+=======
+	while (LastStart <= n) {
+		if (Visited[LastStart] == 0) // 1ë¶€í„° ì˜¬ë¼ê°€ì„œ ë°©ë¬¸ì•ˆí•œ ë…¸ë“œ íƒìƒ‰
+>>>>>>> Stashed changes
 			return LastStart;
 		else
 			LastStart++;
@@ -55,6 +69,7 @@ int NextStart() {
 	return -1;
 }
 int main() {
+
 	int i,x,y,cur,s;
 	scanf("%d %d", &n, &m);
 	for (i = 0; i < m; i++) {
@@ -62,31 +77,39 @@ int main() {
 		SetLink(x, y);
 		SetLink(y, x);
 	}
+<<<<<<< Updated upstream
 	while ((cur = NextStart()) != -1) { // ¹æ¹®ÇÏÁö ¾ÊÀº ³ëµå Å½»ö, Ã³À½ laststart´Â 0
 		printf("%d", cur); // ¹æ¹®¾ÈÇÑ ³ëµå°¡ ÇöÀç³ëµå´Ï±î Ãâ·Â
 		Mark(cur); // ¹æ¹®Çß´Ù°í ¸¶Å©
+=======
+
+	while ((cur = NextStart()) != -1) { // ë°©ë¬¸í•˜ì§€ ì•Šì€ ë…¸ë“œ íƒìƒ‰
+		printf("%d", cur); // ë°©ë¬¸ì•ˆí•œ ë…¸ë“œê°€ í˜„ì¬ë…¸ë“œë‹ˆê¹Œ ì¶œë ¥
+		Mark(cur); // ë°©ë¬¸í–ˆë‹¤ê³  ë§ˆí¬
+>>>>>>> Stashed changes
 		while (1) {
-			if ((s = NextForward(cur)) != -1) { // ³ëµå ±âÁØÀ¸·Î ¿¬°áµÈ ³ëµå Å½»ö
-				if (isMarked(s) == 0) { // ±× ¿¬°áµÈ ³ëµå¸¦ ¹æ¹®Çß´Â°¡?
-					printf(" %d", s); Mark(s); // ¹æ¹®¾ÈÇßÀ¸¸é Ãâ·Â, ¹æ¹®Çß´Ù°í ¸¶Å©
-					Push(cur); // ½ºÅÃ¿¡ ³Ö±â
-					cur = s; // ¹æ¹®ÇÑ ³ëµå¸¦ ´Ù½Ã ÇöÀç³ëµå·Î
+			if ((s = NextForward(cur)) != -1) { // ë…¸ë“œ ê¸°ì¤€ìœ¼ë¡œ ì—°ê²°ëœ ë…¸ë“œ íƒìƒ‰
+				if (isMarked(s) == 0) { // ê·¸ ì—°ê²°ëœ ë…¸ë“œë¥¼ ë°©ë¬¸í–ˆëŠ”ê°€?
+					printf(" %d", s); Mark(s); // ë°©ë¬¸ì•ˆí–ˆìœ¼ë©´ ì¶œë ¥, ë°©ë¬¸í–ˆë‹¤ê³  ë§ˆí¬
+					Push(cur); // ìŠ¤íƒì— ë„£ê¸°
+					cur = s; // ë°©ë¬¸í•œ ë…¸ë“œë¥¼ ë‹¤ì‹œ í˜„ì¬ë…¸ë“œë¡œ
 				}
 				else {
 
 				}
 			}
-			else { // ¿¬°áµÈ ³ëµå°¡ ¾ø´Ù
-				if (isEmpty() == 1) // ½ºÅÃµµ ºñ¾ú³ª?
-					break; // ºñ¾úÀ¸¸é break;
+			else { // ì—°ê²°ëœ ë…¸ë“œê°€ ì—†ë‹¤
+				if (isEmpty() == 1) // ìŠ¤íƒë„ ë¹„ì—ˆë‚˜?
+					break; // ë¹„ì—ˆìœ¼ë©´ break;
 				else
-					cur = Pop(); // ½ºÅÃ¿¡¼­ ÆË (ÀÌÀü ³ëµå·Î ÀÌµ¿)
+					cur = Pop(); // ìŠ¤íƒì—ì„œ íŒ (ì´ì „ ë…¸ë“œë¡œ ì´ë™)
 			}
 		}
+
+
 		printf("\n");
 	}
 
-	
 	return 0;
 }
 
