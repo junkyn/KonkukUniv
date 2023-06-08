907bcfd0991dc6783f651d514945d12c0bbc974c
sterdsterd <sterdsterd@icloud.com>
2023-06-08 13:34:34 +0900

    Rollback

--- src/kucse/introductoryproject/b01/observer/ObservableContactHashSet.java
+++ src/kucse/introductoryproject/b01/observer/ObservableContactHashSet.java
@@ -1,15 +1,15 @@
 package kucse.introductoryproject.b01.observer;
 
-import kucse.introductoryproject.b01.dto.Contact;
-import kucse.introductoryproject.b01.utils.StringUtil;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import kucse.introductoryproject.b01.dto.Contact;
+import kucse.introductoryproject.b01.utils.StringUtil;
 
 public class ObservableContactHashSet extends HashSet<Contact> implements Observable, Observer {
+
     private final List<Observer> observers = new ArrayList<>();
 
     @Override
@@ -57,27 +57,35 @@ public class ObservableContactHashSet extends HashSet<Contact> implements Observ
     }
 
     public boolean isPhoneDuplicated(String phone) {
-        return this.stream().anyMatch(it -> StringUtil.getNumbersOnly(it.getPhone()).equals(StringUtil.getNumbersOnly(phone)));
+        return this.stream().anyMatch(it -> StringUtil.getNumbersOnly(it.getPhone())
+            .equals(StringUtil.getNumbersOnly(phone)));
     }
 
     public String countName(String name) {
         AtomicInteger count = new AtomicInteger();
-        if (this.stream().anyMatch(it -> it.getName().equals(name)))
+        if (this.stream().anyMatch(it -> it.getName().equals(name))) {
             count.incrementAndGet();
+        }
 
-        while (count.get() > 0 && this.stream().anyMatch(it -> it.getName().equals(name + "(" + count.get() + ")")))
+        while (count.get() > 0 && this.stream()
+            .anyMatch(it -> it.getName().equals(name + "(" + count.get() + ")"))) {
             count.incrementAndGet();
+        }
 
-        return count.get() == 0 ? name : name + "[" + count.get() + "]";
+        return count.get() == 0 ? name : name + "(" + count.get() + ")";
     }
 
     public String renameFrom(String fromName, String toName) {
         AtomicInteger count = new AtomicInteger();
-        if (this.stream().filter(it -> !it.getName().equals(fromName)).anyMatch(it -> it.getName().equals(toName)))
+        if (this.stream().filter(it -> !it.getName().equals(fromName))
+            .anyMatch(it -> it.getName().equals(toName))) {
             count.incrementAndGet();
+        }
 
-        while (count.get() > 0 && this.stream().filter(it -> !it.getName().equals(fromName)).anyMatch(it -> it.getName().equals(toName + "(" + count.get() + ")")))
+        while (count.get() > 0 && this.stream().filter(it -> !it.getName().equals(fromName))
+            .anyMatch(it -> it.getName().equals(toName + "(" + count.get() + ")"))) {
             count.incrementAndGet();
+        }
 
         return count.get() == 0 ? toName : toName + "(" + count.get() + ")";
     }
